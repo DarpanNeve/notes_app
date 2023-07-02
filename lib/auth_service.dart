7cbbee8bc6ede0002b6f34b3feea40fcf1424d35
Darpan Neve <Darpanneve3@gmail.com>
2023-07-02 16:19:43 +0530

    sorted sign in problem for flutter web

--- lib/auth_service.dart
+++ lib/auth_service.dart
@@ -45,7 +45,7 @@ class AuthService {
 
       googleProvider
           .addScope('https://www.googleapis.com/auth/contacts.readonly');
-      googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+      googleProvider.setCustomParameters({'login_hint':'user@example.com'});
       return await FirebaseAuth.instance.signInWithPopup(googleProvider);
     }
   }
